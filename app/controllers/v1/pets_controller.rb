b4eed1eae8994f6102143f9b02e1e617159d3f3e
Serlle Rosales <jesus.rosles@concretosdcc.com>
2021-07-30 19:49:39 -0500

    Programar paginado y headers, modificacion de accionadores

--- app/controllers/v1/pets_controller.rb
+++ app/controllers/v1/pets_controller.rb
@@ -1,16 +1,25 @@
 class V1::PetsController < ApplicationController
   before_action :set_pet, only: [:show]
+  DEFAULT_PETS_LIMIT = 10
 
   # GET /v1/pets
   def index
-    @pets = Pet.all
+    collection_all = Pet.all
+    pet = Pet.find_by(id: params[:cursor])
+    collection = collection_all.where("id > ?", pet.id) if pet.present?
+    collection = collection.limit(limit)
+    binding.irb
+    response.headers["X-Next"] = v1_pets_url(limit: limit, cursor: collection.last.id)
+    render json: collection.to_json(except: [:created_at, :updated_at])
 
-    render json: @pets.to_json(except: [:created_at, :updated_at])
+    # @pets = Pet.all.limit(params[:limit].present? ? params[:limit].to_i : DEFAULT_PETS_LIMIT)
+    #
+    # render json: @pets.to_json(except: [:created_at, :updated_at])
   end
 
   # GET /v1/pets/1
   def show
-    render json: @pets.to_json(except: [:created_at, :updated_at])
+    render json: @pet.to_json(except: [:created_at, :updated_at])
   end
 
   # POST /v1/pets
@@ -25,13 +34,18 @@ class V1::PetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pet
-      @pet = Pet.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def pet_params
-      params.permit(:name, :tag)
-    end
+  def limit
+    params[:limit].present? ? params[:limit].to_i : DEFAULT_PETS_LIMIT
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def pet_params
+    params.permit(:name, :tag)
+  end
 end
